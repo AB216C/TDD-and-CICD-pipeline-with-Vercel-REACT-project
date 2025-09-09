44e84f89b7c589912fbcefd4b22a53f3c19ffd2e
Ferdinand Niyongire <ferdinandniyongira@gmail.com>
2025-09-08 21:59:36 -0500

    CI CD Updates

--- E-commerce-app/jest.config.js
+++ E-commerce-app/jest.config.js
@@ -1,21 +1,3 @@
-
-// export default {
-//     preset: 'ts-jest',
-//     testEnvironment: 'jsdom',
-//     transform: {
-//     '^.+\\.(ts|tsx)$': 'ts-jest',
-//     '^.+\\.(js|jsx)$': 'babel-jest',
-//     },
-//     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-//     testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
-//     moduleNameMapper: {
-//     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-//     },
-// };
-
-
-
-
 export default  {
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
